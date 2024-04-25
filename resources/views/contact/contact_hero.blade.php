b68094c35c768fc13386c843093540a67e6aebf8
devFarjanur <farjanur15-2956@diu.edu.bd>
2024-04-25 20:02:41 +0600

    Business Company Portfolio

--- resources/views/contact/contact_hero.blade.php
+++ resources/views/contact/contact_hero.blade.php
@@ -27,7 +27,7 @@
 
 
       <!-- ======= Breadcrumbs ======= -->
-    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('assets/img/breadcrumbs-bg.jpg');">
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('backend/assets/img/breadcrumbs-bg.jpg');">
       <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
 
         <h2>Contact</h2>
